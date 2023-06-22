591e398f07ecad1025d01586da8c9232c89ce827
João Victor <52728391+JoaoVic7or@users.noreply.github.com>
2023-06-21 22:16:05 -0300

    update

---
+++ src/firebaseconfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC22yQmS3JTpuNngmvAko7gKbsnY5yp-lI",
+    authDomain: "portfolio-4cf33.firebaseapp.com",
+    databaseURL: "https://portfolio-4cf33-default-rtdb.firebaseio.com",
+    projectId: "portfolio-4cf33",
+    storageBucket: "portfolio-4cf33.appspot.com",
+    messagingSenderId: "251780444282",
+    appId: "1:251780444282:web:8103b6f004c61e03458bc3",
+    measurementId: "G-BDEBLV6HNJ"
+};
+
+export default firebaseConfig;
